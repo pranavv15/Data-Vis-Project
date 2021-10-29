8b584ef25fd925a6c7b60908fd5103e26855b2a6
pranavv15 <vinodpranav15@gmail.com>
2021-10-29 12:47:11 -0400

    error with grouping

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+from os import replace
 import numpy as np
 import pandas as pd
 
@@ -8,7 +9,16 @@ df = pd.read_csv(data)
 df = df.drop('Notes', axis=1)
 df = df.drop(['Sex Code','States Code'], axis=1)
 df = df.dropna()
+rep = ["District of Columbia","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Rhode Island","South Carolina","South Dakota","West Virginia"]
+val = ["District-of-Columbia","New-Hampshire","New-Jersey","New-Mexico","New-York","North-Carolina","North-Dakota","Rhode-Island","South-Carolina","South-Dakota","West-Virginia"]
+
+df = df.replace(to_replace=rep, value=val)
+
 df1 = df.pivot(index= "States", columns="Sex",values="Count")
-print(df.head())
+df1.reset_index(inplace=True)
+
+print(df.head(5))
+# print(df1.head(20))
 print(df1.head(20))
+
 df1.to_csv("/Users/pranavvinod/Documents/GitHub/Data-Vis-Project/Cancer-Data.csv")
