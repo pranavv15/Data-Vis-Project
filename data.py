8471fb2e63a780872fa0126cbdf757294664631c
pranavv15 <vinodpranav15@gmail.com>
2021-11-29 00:35:07 -0500

    Data Added

--- data.py
+++ data.py
@@ -4,22 +4,25 @@ import pandas as pd
 
 # Program to modify data
 
-data = "United States and Puerto Rico Cancer Statistics, 1999-2017 Incidence Archive-2.csv"
+data = "data vis data/obesity_by_sex.csv"
 df = pd.read_csv(data)
-
+print(df.head(5))
 # Dropping 3 columns and na values
-df = df.drop('Notes', axis=1)
-df = df.drop(['Sex Code','States Code'], axis=1)
+df = df.drop('Footnotes', axis=1)
+# df = df.drop(['Sex Code','States Code'], axis=1)
 df = df.dropna()
+print(df.head(5))
+
 
 # Replacing spaces with "-"
-rep = ["District of Columbia","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Rhode Island","South Carolina","South Dakota","West Virginia"]
-val = ["District-of-Columbia","New-Hampshire","New-Jersey","New-Mexico","New-York","North-Carolina","North-Dakota","Rhode-Island","South-Carolina","South-Dakota","West-Virginia"]
+rep = ["United States","District of Columbia","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Rhode Island","South Carolina","South Dakota","West Virginia"]
+val = ["United-States","District-of-Columbia","New-Hampshire","New-Jersey","New-Mexico","New-York","North-Carolina","North-Dakota","Rhode-Island","South-Carolina","South-Dakota","West-Virginia"]
 df = df.replace(to_replace=rep, value=val)
 
 # Creating a table containing count of male and female cancer patients
-df1 = df.pivot(index= "States", columns="Sex",values="Count")
-df1.reset_index(inplace=True)
+# df1 = df.pivot(index= "Location", columns="Sex",values="Count")
+# df1.reset_index(inplace=True)
 
-# Saving to another csv file
-df1.to_csv("/Users/pranavvinod/Documents/GitHub/Data-Vis-Project/Cancer-Data.csv")
+# # Saving to another csv file
+df.to_csv("/Users/pranavvinod/Documents/GitHub/Data-Vis-Project/Obesity-Data-Sex.csv")
+print(df.head(5))
